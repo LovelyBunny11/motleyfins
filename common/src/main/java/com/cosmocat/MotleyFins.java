c2435a6704173517bd16d1e3de4f99d9fed1abc4
LB2345 <163643124+LovelyBunny11@users.noreply.github.com>
2026-02-02 09:45:30 +0300

    Initial commit

---
+++ common/src/main/java/com/cosmocat/MotleyFins.java
@@ -0,0 +1,9 @@
+package com.cosmocat;
+
+public final class MotleyFins {
+    public static final String MOD_ID = "motleyfins";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
